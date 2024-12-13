3282369487dd8cd81324ae932ed296d3c129830a
Stuartferguson00 <99267485+Stuartferguson00@users.noreply.github.com>
2024-12-13 13:38:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='cgqemcmc',
-    version='0.3.2',  
+    version='0.3.3',  
     packages=['cgqemcmc'],  
     include_package_data=True,
-    #install_requires=['numpy>=1.14.5'],# Ignores install rezquirements for now
     author='S. Ferguson',
-    author_email='s1846096@ed.ac.uk',
+    author_email='S.S.Ferguson-3@ed.ac.uk6@ed.ac.uk',
     description='Code that coarse-grains the QeMCMC',
 
 )
